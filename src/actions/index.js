9a08ff9208fd763e730d5d751dfbfc41e4c74da1
Thom Allen <thomwork@gmail.com>
2017-11-20 12:30:42 +0000

    Does help to actually assign a value to constants...

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
-export const GIF_SEARCH_REQUEST;
-export const GIF_SEARCH_SUCCESS;
-export const GIF_SEARCH_FAILURE;
+export const GIF_SEARCH_REQUEST = 'GIF_SEARCH_REQUEST';
+export const GIF_SEARCH_SUCCESS = 'GIF_SEARCH_SUCCESS';
+export const GIF_SEARCH_FAILURE = 'GIF_SEARCH_FAILURE';
 
 export const searchGifs = (dispatch) => {
   dispatch({ type: GIF_SEARCH_REQUEST });
