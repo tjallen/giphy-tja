ffaac7a066bed9a61f7c5c657a7fff780cb1437f
Thom Allen <thomwork@gmail.com>
2017-11-20 11:48:29 +0000

    Add placeholder query

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import './App.css';
 import keys from './keys.json';
 
 class App extends Component {
+  componentDidMount() {
+    const query = 'placeholder';
+    const request = `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${keys.giphy}`;
+    fetch(request)
+      .then(res => res.json())
+      .then(results => console.log(results.data))
+      .then(results => this.setState({
+        results
+      }));
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcomes</h1>
         </header>
-        <p>{keys.giphy || 'no keys'}</p>
       </div>
     );
   }
