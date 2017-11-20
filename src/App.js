2b586c50fb5de36efa3b0d7648bf25336940b5aa
Thom Allen <thomwork@gmail.com>
2017-11-20 11:41:41 +0000

    Add api keys in gitignored src/keys.json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import keys from './keys.json';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Welcomes</h1>
         </header>
+        <p>{keys.giphy || 'no keys'}</p>
       </div>
     );
   }
