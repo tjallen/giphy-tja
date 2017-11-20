a4b4ea038991ca3734a302cc180d5b130a6cad02
Thom Allen <thomwork@gmail.com>
2017-11-20 13:02:19 +0000

    Add basic reducer cases for GIF_SEARCH_* actions

--- src/reducers/gifSearch.js
+++ src/reducers/gifSearch.js
@@ -1,3 +1,9 @@
+import {
+  GIF_SEARCH_REQUEST,
+  GIF_SEARCH_SUCCESS,
+  GIF_SEARCH_FAILURE,
+} from './../actions';
+
 const initialState = {
   isFetching: false,
   results: [],
@@ -5,6 +11,25 @@ const initialState = {
 
 const gifs = (state = initialState, action) => {
   switch(action.type) {
+    case GIF_SEARCH_REQUEST: {
+      return Object.assign({}, state, {
+        isFetching: true,
+        results: state.results,
+      });
+    }
+    case GIF_SEARCH_SUCCESS: {
+      return Object.assign({}, state, {
+        isFetching: false,
+        results: action.data,
+      });
+    }
+    case GIF_SEARCH_FAILURE: {
+      return Object.assign({}, state, {
+        isFetching: false,
+        results: [],
+        error: action.err,
+      })
+    }
     default:
       return state;
   }
