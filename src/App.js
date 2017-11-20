6daa007da9517878751e61a1213e99a6d80c6dca
Thom Allen <thomwork@gmail.com>
2017-11-20 11:54:43 +0000

    Update placeholder to map gif ids to render

--- src/App.js
+++ src/App.js
@@ -3,22 +3,31 @@ import './App.css';
 import keys from './keys.json';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: [],
+    }
+  }
   componentDidMount() {
     const query = 'placeholder';
     const request = `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${keys.giphy}`;
     fetch(request)
       .then(res => res.json())
-      .then(results => console.log(results.data))
-      .then(results => this.setState({
-        results
+      .then(res => this.setState({
+        results: res.data,
       }));
   }
   render() {
+    const { results } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcomes</h1>
         </header>
+        {results.map((item, index) =>
+          <li key={index}>{item.id}</li>
+        )}
       </div>
     );
   }
