bd6e41e50f35a971a56c287aae5ffa4a98ae45b3
Thom Allen <thomwork@gmail.com>
2017-11-20 13:37:21 +0000

    Add &limit=10 to api request

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,7 @@ export const GIF_SEARCH_SUCCESS = 'GIF_SEARCH_SUCCESS';
 export const GIF_SEARCH_FAILURE = 'GIF_SEARCH_FAILURE';
 
 export const searchGifs = (query) => {
-  const request = `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${keys.giphy}`;
+  const request = `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${keys.giphy}&limit=10`;
   return function(dispatch) {
   dispatch({ type: GIF_SEARCH_REQUEST });
   return fetch(request)
