0ba3bde41f914ca7545eff293294799ad762a201
Thom Allen <thomwork@gmail.com>
2017-11-20 15:54:00 +0000

    Intendation

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,13 +7,12 @@ export const GIF_SEARCH_FAILURE = 'GIF_SEARCH_FAILURE';
 export const searchGifs = (query) => {
   const request = `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${keys.giphy}&limit=10`;
   return function(dispatch) {
-  dispatch({ type: GIF_SEARCH_REQUEST });
-  return fetch(request)
-    .then(response => response.json())
-    .then(
-      response => dispatch({ type: GIF_SEARCH_SUCCESS, response }),
-      error => dispatch({ type: GIF_SEARCH_FAILURE, error })
-    );
+    dispatch({ type: GIF_SEARCH_REQUEST });
+    return fetch(request)
+      .then(response => response.json())
+      .then(
+        response => dispatch({ type: GIF_SEARCH_SUCCESS, response }),
+        error => dispatch({ type: GIF_SEARCH_FAILURE, error })
+      );
   }
-
 }
