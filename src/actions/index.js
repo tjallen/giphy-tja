a1ed7483d42cb4d6c99cbe7bd3c22d92ebdde9c6
Thom Allen <thomwork@gmail.com>
2017-11-20 12:15:24 +0000

    Redux BP: actions

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+export const GIF_SEARCH_REQUEST;
+export const GIF_SEARCH_SUCCESS;
+export const GIF_SEARCH_FAILURE;
+
+export const searchGifs = (dispatch) => {
+  dispatch({ type: GIF_SEARCH_REQUEST });
+  return fetch(`req url`)
+    .then(res => res.json())
+    .then(
+      data => dispatch({ type: GIF_SEARCH_SUCCESS, data }),
+      err => dispatch({ type: GIF_SEARCH_FAILURE, err })
+    );
+}
